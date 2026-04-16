1be6e8a356aa4d3112b6d20d911117babbaf10f8
Davis Sneed <101902819+dsneed123@users.noreply.github.com>
2026-04-15 23:57:38 -0700

    Create logout view (#14)
    Co-authored-by: dsneed123 <davissneed@Daviss-Mac-mini.local>

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
