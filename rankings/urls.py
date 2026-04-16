676fdb1be1f3c23b154b35b645d8d19931a509ee
dsneed123 <davissneed@Daviss-Mac-mini.local>
2026-04-16 05:28:36 -0700

    Add purchase regret tracker

--- rankings/urls.py
+++ rankings/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('rankings/', views.rankings, name='rankings'),
     path('rankings/review/', views.rankings_review, name='rankings_review'),
+    path('rankings/review/regret/', views.rankings_review_regret, name='rankings_review_regret'),
 ]
