8bb056dc94b1956b61d633580285036e078de37d
dsneed123 <davissneed@Daviss-Mac-mini.local>
2026-04-16 00:40:23 -0700

    Add balance history chart on account detail

--- banking/urls.py
+++ banking/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('accounts/', views.account_list, name='account_list'),
     path('accounts/add/', views.account_add, name='account_add'),
+    path('accounts/<int:account_id>/', views.account_detail, name='account_detail'),
     path('accounts/<int:account_id>/edit/', views.account_edit, name='account_edit'),
     path('accounts/<int:account_id>/update-balance/', views.account_update_balance, name='account_update_balance'),
 ]
