1aa61a7765fe94495e998cdc57be85b2334f444b
dsneed123 <davissneed@Daviss-Mac-mini.local>
2026-04-15 23:59:38 -0700

    Create user profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
