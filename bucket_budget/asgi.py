d5eba248369b4220454f8e671a7668151ca7008b
Davis Sneed <101902819+dsneed123@users.noreply.github.com>
2026-04-15 23:38:45 -0700

    Initialize Django project structure (#1)
    Co-authored-by: dsneed123 <davissneed@Daviss-Mac-mini.local>

---
+++ bucket_budget/asgi.py
@@ -0,0 +1,8 @@
+"""ASGI config for bucket_budget project."""
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bucket_budget.settings')
+
+application = get_asgi_application()
