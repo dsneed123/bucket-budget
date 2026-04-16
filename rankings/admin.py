f27ef18e353df75031ff899f7a65c837460c76c6
dsneed123 <davissneed@Daviss-Mac-mini.local>
2026-04-16 04:54:14 -0700

    Create NecessitySnapshot model

--- rankings/admin.py
+++ rankings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import NecessitySnapshot
+
+
+@admin.register(NecessitySnapshot)
+class NecessitySnapshotAdmin(admin.ModelAdmin):
+    list_display = ('user', 'period_start', 'period_end', 'avg_score', 'total_spend', 'transaction_count', 'created_at')
+    list_filter = ('user',)
+    ordering = ('-period_start',)
