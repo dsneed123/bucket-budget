141a207c8fc9b1ae27c947476daa29e678650bb9
dsneed123 <davissneed@Daviss-Mac-mini.local>
2026-04-16 00:28:41 -0700

    Add bank accounts list view

--- banking/views.py
+++ banking/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import BankAccount
+
+
+@login_required
+def account_list(request):
+    accounts = BankAccount.objects.filter(user=request.user, is_active=True).order_by('name')
+    total_balance = sum(a.balance for a in accounts)
+    return render(request, 'banking/account_list.html', {
+        'accounts': accounts,
+        'total_balance': total_balance,
+    })
