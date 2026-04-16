c7755161c8961aa7acf38fcddb013209bff41b5f
Davis Sneed <101902819+dsneed123@users.noreply.github.com>
2026-04-16 00:31:48 -0700

    Add create/edit bank account views (#29)
    Co-authored-by: dsneed123 <davissneed@Daviss-Mac-mini.local>

--- banking/urls.py
+++ banking/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('accounts/', views.account_list, name='account_list'),
+    path('accounts/add/', views.account_add, name='account_add'),
+    path('accounts/<int:account_id>/edit/', views.account_edit, name='account_edit'),
 ]
