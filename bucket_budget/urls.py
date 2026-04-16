7dac62593645139de7b178e3f289bc79c9483f13
dsneed123 <davissneed@Daviss-Mac-mini.local>
2026-04-16 02:02:05 -0700

    Create add transaction view

--- bucket_budget/urls.py
+++ bucket_budget/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('', include('banking.urls')),
     path('', include('buckets.urls')),
+    path('', include('transactions.urls')),
 ]
