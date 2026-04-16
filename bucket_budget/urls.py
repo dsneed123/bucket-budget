f180244b483c9644bb2d21a1252f0abb134cfadb
dsneed123 <davissneed@Daviss-Mac-mini.local>
2026-04-15 23:44:07 -0700

    Create core app with landing page

--- bucket_budget/urls.py
+++ bucket_budget/urls.py
@@ -1,7 +1,8 @@
 """URL configuration for bucket_budget project."""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
