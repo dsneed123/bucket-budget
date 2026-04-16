fdfd02067eba2349b3036aa43f19faf7f47655ea
dsneed123 <davissneed@Daviss-Mac-mini.local>
2026-04-15 23:45:06 -0700

    Add health check endpoint

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'core/index.html')
+
+
+def health(request):
+    return HttpResponse("ok", status=200)
