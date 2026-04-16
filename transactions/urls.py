30bf11277072570a2fea419ad167868b9363afe0
Davis Sneed <101902819+dsneed123@users.noreply.github.com>
2026-04-16 03:04:52 -0700

    Add transaction detail view (#62)
    Co-authored-by: dsneed123 <davissneed@Daviss-Mac-mini.local>

--- transactions/urls.py
+++ transactions/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('transactions/add/', views.transaction_add, name='transaction_add'),
     path('transactions/add/split/', views.transaction_add_split, name='transaction_add_split'),
     path('transactions/transfer/', views.transaction_transfer, name='transaction_transfer'),
+    path('transactions/<int:transaction_id>/', views.transaction_detail, name='transaction_detail'),
     path('transactions/<int:transaction_id>/edit/', views.transaction_edit, name='transaction_edit'),
     path('transactions/<int:transaction_id>/delete/', views.transaction_delete, name='transaction_delete'),
 ]
