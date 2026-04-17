994b61e33424197bfc2155ecd6afe4b9b0017e75
Davis Sneed <101902819+dsneed123@users.noreply.github.com>
2026-04-16 19:39:29 -0700

    Add sitemap and robots.txt (#190)
    Co-authored-by: dsneed123 <davissneed@Daviss-Mac-mini.local>

--- bucket_budget/urls.py
+++ bucket_budget/urls.py
@@ -2,14 +2,24 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from django.views.generic import TemplateView
+
+from core.sitemaps import StaticViewSitemap
 
 handler403 = 'django.views.defaults.permission_denied'
 handler404 = 'django.views.defaults.page_not_found'
 handler500 = 'django.views.defaults.server_error'
 
+sitemaps = {
+    'static': StaticViewSitemap,
+}
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     path('', include('core.urls')),
     path('', include('accounts.urls')),
     path('', include('banking.urls')),
