1f554c5940f7abd14d4604569b30d25b800d75e2
dsneed123 <davissneed@Daviss-Mac-mini.local>
2026-04-17 09:59:10 -0700

    Fix sidebar active link detection

--- core/templatetags/breadcrumb_tags.py
+++ core/templatetags/breadcrumb_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.inclusion_tag('core/breadcrumbs.html')
 def render_breadcrumbs(breadcrumbs):
     return {'breadcrumbs': breadcrumbs}
+
+
+@register.filter
+def startswith(value, prefix):
+    return str(value).startswith(prefix)
